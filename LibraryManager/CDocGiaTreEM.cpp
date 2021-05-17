735459421fd37308adca5d26825035557b8fcf14
congminh1809 <congminh1809@gmail.com>
2021-05-17 17:11:28 +0700

    plzz

---
+++ LibraryManager/CDocGiaTreEM.cpp
@@ -0,0 +1,20 @@
+﻿#include "CDocGiaTreEM.h"
+
+// phương thức nhập thông tin độc giả trẻ em
+void CDocGiaTreEM::Input()
+{
+	// gọi lại phương thức nhập ở class cha
+	CDocGia::Input();
+
+
+	cin.ignore();
+	cout << endl << "Nhap ho ten nguoi dai dien: ";
+	getline(cin, hotenNDD);
+}
+	
+
+// phương thức xuất thông tin độc giả trẻ em
+void CDocGiaTreEM::Output()
+{
+	
+}
